24f4e0e55a799ad7e2ea4b6d4ae304d28d02c352
ijemmy <ijemmy@users.noreply.github.com>
2014-12-14 23:26:03 +0700

    3.4 Render feature container
    - Add html structure of SPA in spa.js
    - Add css of the structure in spa.shell.css

--- js/spa.js
+++ js/spa.js
@@ -4,8 +4,36 @@
  */
 
 var spa = (function () {
-    var initModule = function ($container) {
-        $container.html('<h1>Hello World</h1>');
+    var configMap = {
+            main_html: '<div class="spa-shell-head">'
+            + '<div class="spa-shell-head-logo"></div>'
+            + '<div class="spa-shell-head-acct"></div>'
+            + '<div class="spa-shell-head-search"></div>'
+            + '</div>'
+            + '<div class="spa-shell-main">'
+            + '<div class="spa-shell-main-nav"></div>'
+            + '<div class="spa-shell-main-content"></div>'
+            + '</div>'
+            + '<div class="spa-shell-foot"></div>'
+            + '<div class="spa-shell-chat"></div>'
+            + '<div class="spa-shell-modal"></div>'
+        },
+        stateMap = {$container: null},
+        jqueryMap = {},
+        setJqueryMap, initModule;
+
+    setJqueryMap = function () {
+        jqueryMap = {
+            $container: stateMap.$container
+        };
     };
-    return {initModule: initModule};
+    initModule = function ($container) {
+        stateMap.$container = $container;
+        $container.html(configMap.main_html);
+        setJqueryMap();
+    };
+    return {
+        setJqueryMap: setJqueryMap,
+        initModule: initModule
+    };
 }());
